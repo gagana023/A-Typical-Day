e8769287cc665d88a647290c4aee880c2b1deed3
gaganabodala <2010941@apps.nsd.org>
2026-06-02 09:18:52 +0000

    dialogue ui

--- src/main/java/app/DialogueUI.java
+++ src/main/java/app/DialogueUI.java
@@ -5,6 +5,7 @@ import javafx.animation.FadeTransition;
 import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
@@ -26,6 +27,8 @@ public class DialogueUI {
   private StackPane response2;
   private StackPane response3;
 
+  private VBox optionBox;
+
   /**
    * Creates a dialogue interface using the given user options and NPC responses.
    *
@@ -37,9 +40,22 @@ public class DialogueUI {
     option2 = new Button(userOptions.get(1));
     option3 = new Button(userOptions.get(2));
 
-    option1.setVisible(false);
-    option2.setVisible(false);
-    option3.setVisible(false);
+    styleOptionButton(option1);
+    styleOptionButton(option2);
+    styleOptionButton(option3);
+
+    optionBox = new VBox(10);
+    optionBox.setPrefSize(620, 170);
+    optionBox.setStyle(
+        "-fx-background-color: rgba(18, 20, 35, 0.94);"
+            + "-fx-background-radius: 18;"
+            + "-fx-border-color: white;"
+            + "-fx-border-width: 2;"
+            + "-fx-border-radius: 18;"
+            + "-fx-padding: 15;");
+
+    optionBox.getChildren().addAll(option1, option2, option3);
+    optionBox.setVisible(false);
 
     response1 = createResponseBubble(npcOptions.get(0));
     response2 = createResponseBubble(npcOptions.get(1));
@@ -53,6 +69,39 @@ public class DialogueUI {
   }
 
   /**
+   * Styles one dialogue option button.
+   *
+   * @param button the button to style
+   */
+  private void styleOptionButton(Button button) {
+    button.setMaxWidth(Double.MAX_VALUE);
+    button.setWrapText(true);
+
+    String normalStyle =
+        "-fx-background-color: #30364f;"
+            + "-fx-text-fill: white;"
+            + "-fx-font-size: 14px;"
+            + "-fx-background-radius: 10;"
+            + "-fx-border-color: #9aa7ff;"
+            + "-fx-border-radius: 10;"
+            + "-fx-padding: 8;";
+
+    String hoverStyle =
+        "-fx-background-color: #4b5bdc;"
+            + "-fx-text-fill: white;"
+            + "-fx-font-size: 14px;"
+            + "-fx-background-radius: 10;"
+            + "-fx-border-color: white;"
+            + "-fx-border-radius: 10;"
+            + "-fx-padding: 8;";
+
+    button.setStyle(normalStyle);
+
+    button.setOnMouseEntered(e -> button.setStyle(hoverStyle));
+    button.setOnMouseExited(e -> button.setStyle(normalStyle));
+  }
+
+  /**
    * Creates a speech bubble for an NPC response.
    *
    * @param responseText the text displayed inside the response bubble
@@ -60,36 +109,33 @@ public class DialogueUI {
    */
   private StackPane createResponseBubble(String responseText) {
     Text text = new Text(responseText);
-    text.setWrappingWidth(240);
+    text.setWrappingWidth(560);
+    text.setFill(Color.WHITE);
+    text.setStyle("-fx-font-size: 16px;");
 
-    Rectangle box = new Rectangle(260, 70);
-    box.setFill(Color.WHITE);
-    box.setStroke(Color.BLACK);
-    box.setArcWidth(15);
-    box.setArcHeight(15);
+    Rectangle box = new Rectangle(620, 120);
+    box.setFill(Color.rgb(18, 20, 35, 0.94));
+    box.setStroke(Color.WHITE);
+    box.setStrokeWidth(2);
+    box.setArcWidth(25);
+    box.setArcHeight(25);
 
     return new StackPane(box, text);
   }
 
   /** Sets the screen positions of the dialogue options and response bubbles. */
   private void setPositions() {
-    AnchorPane.setTopAnchor(response1, 100.0);
-    AnchorPane.setLeftAnchor(response1, 250.0);
+    AnchorPane.setTopAnchor(optionBox, 390.0);
+    AnchorPane.setLeftAnchor(optionBox, 90.0);
 
-    AnchorPane.setTopAnchor(response2, 100.0);
-    AnchorPane.setLeftAnchor(response2, 250.0);
+    AnchorPane.setTopAnchor(response1, 400.0);
+    AnchorPane.setLeftAnchor(response1, 90.0);
 
-    AnchorPane.setTopAnchor(response3, 100.0);
-    AnchorPane.setLeftAnchor(response3, 250.0);
+    AnchorPane.setTopAnchor(response2, 400.0);
+    AnchorPane.setLeftAnchor(response2, 90.0);
 
-    AnchorPane.setTopAnchor(option1, 200.0);
-    AnchorPane.setLeftAnchor(option1, 300.0);
-
-    AnchorPane.setTopAnchor(option2, 250.0);
-    AnchorPane.setLeftAnchor(option2, 300.0);
-
-    AnchorPane.setTopAnchor(option3, 300.0);
-    AnchorPane.setLeftAnchor(option3, 300.0);
+    AnchorPane.setTopAnchor(response3, 400.0);
+    AnchorPane.setLeftAnchor(response3, 90.0);
   }
 
   /**
@@ -98,43 +144,55 @@ public class DialogueUI {
    * @param root the AnchorPane that receives the dialogue UI elements
    */
   public void addToRoot(AnchorPane root) {
-    root.getChildren().addAll(option1, option2, option3, response1, response2, response3);
+    root.getChildren().addAll(optionBox, response1, response2, response3);
   }
 
   /** Shows the dialogue option buttons and starts their fade animations. */
   public void showOptions() {
+    response1.setVisible(false);
+    response2.setVisible(false);
+    response3.setVisible(false);
+
+    optionBox.setOpacity(1);
+    optionBox.setVisible(true);
+
     option1.setOpacity(1);
     option2.setOpacity(1);
     option3.setOpacity(1);
 
-    option1.setVisible(true);
-    option2.setVisible(true);
-    option3.setVisible(true);
+    option1.setDisable(false);
+    option2.setDisable(false);
+    option3.setDisable(false);
 
     startOptionFades();
   }
 
   /** Hides all dialogue option buttons. */
   public void hideOptions() {
-    option1.setVisible(false);
-    option2.setVisible(false);
-    option3.setVisible(false);
+    optionBox.setVisible(false);
   }
 
   /**
-   * Starts fade animations for each dialogue option button.
+   * Starts fade animations for the whole option box.
    *
-   * <p>Each option fades at a different speed so the choices disappear gradually.
+   * <p>The option box fades out if the player waits too long.
    */
   private void startOptionFades() {
     FadeTransition fade1 = new FadeTransition(Duration.seconds(3.5), option1);
     fade1.setToValue(0);
+    fade1.setOnFinished(e -> option1.setDisable(true));
 
     FadeTransition fade2 = new FadeTransition(Duration.seconds(7), option2);
     fade2.setToValue(0);
+    fade2.setOnFinished(e -> option2.setDisable(true));
 
     FadeTransition fade3 = new FadeTransition(Duration.seconds(10), option3);
     fade3.setToValue(0);
+    fade3.setOnFinished(
+        e -> {
+          option3.setDisable(true);
+          optionBox.setVisible(false);
+        });
 
     fade1.play();
     fade2.play();
